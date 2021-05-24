abf85c516be2361139cbf6b06dfad580bbd36059
Dylan Mikus <dbmikus@gmail.com>
2021-05-23 20:01:56 -0700

    Cleanup packages/frontend
    packages/frontend was imported from another template repo, so it has
    some cruft from when it was a standalone repo. We clean that up,
    preferring certain configuration at the Lerna root (aka the repo root).

--- husky.d/pre-push.frontend.sh
+++ husky.d/pre-push.frontend.sh
@@ -6,5 +6,9 @@ cd "$(dirname "$0")"/..
 
 last_cmd=$(echo "$HUSKY_GIT_STDIN" | grep -E '^\(delete\)' > /dev/null; echo $?)
 if [ $last_cmd != 0 ] ; then
-    true
+    pushd packages/frontend
+    npm run lint
+    npm run ts:type-check
+    npm run test
+    popd
 fi
